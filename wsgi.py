822553250fcd29bf495ae1ffaf15c8f71abef374
Hola-py-thon <shrkiop1@gmail.com>
2025-01-09 17:34:46 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# The app should listen on the port specified by the environment variable
-port = int(os.environ.get("PORT", 10000))  # Default to 5000 if not set
-
 if __name__ == "__main__":
-    # Make sure the app listens on the right port
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=10000)
