53c0dfac417ba5b18a1cea47b71b94e0f1d9a024
Hola-py-thon <shrkiop1@gmail.com>
2025-01-09 16:49:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.py import app  # Import the Flask app from your main file (replace 'your_flask_file' with your actual file name)
+from flask import app  # Import the Flask app from your main file (replace 'your_flask_file' with your actual file name)
 
 # Gunicorn will use this app instance to start the server
 if __name__ == "__main__":
